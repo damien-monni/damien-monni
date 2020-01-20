01d269d17dbcef6e1981dff0c453faccaee3e274
Damien Monni <contact@damien-monni.com>
2020-01-20 19:18:04 +0100

    Add meta elements

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,8 +8,16 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="fr">
         <Head />
+
+        <title>Damien Monni - Développeur web et mobile freelance</title>
+
+        <description>
+          Je m'appelle Damien et je suis développeur web freelance, full-stack
+          et spécialisé dans les technologies modernes, principalement liées au
+          JavaScript.
+        </description>
 
         <link
           href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap"
