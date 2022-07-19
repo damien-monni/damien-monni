a4c9bdadc8905448a1f48ee2f42142af3458c36e
Damien Monni <contact@damien-monni.com>
2022-07-19 15:20:11 +0200

    Replace v2 by a new Next.js application

--- src/components/Portfolio/Tech.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import styles from './Tech.module.css';
-
-const Tech = ({ tech }) => {
-  return <span className={styles.root}>{tech.label}</span>;
-};
-
-export default Tech;
