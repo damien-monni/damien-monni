918998d8b43021d9560e4c2d4b7d0fa4d77ca01d
Damien Monni <contact@damien-monni.com>
2022-07-27 22:15:07 +0200

    Add skill chips to the "me" picture

--- src/components/Hero/MePicture.tsx
+++ src/components/Hero/MePicture.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx';
 import Image from 'next/image';
 import mePicture from '../../../public/img/me.png';
+import SkillChip from './SkillChip';
 
 interface MePictureProps {
   className?: string;
@@ -15,6 +16,35 @@ export default function MePicture({ className }: MePictureProps) {
 
         {/* Picture */}
         <Image src={mePicture} alt="Damien Monni" />
+
+        {/****** Skill chips ******/}
+
+        {/* React */}
+        <SkillChip
+          className="absolute top-4 left-0"
+          src="/img/react-logo.svg"
+        />
+
+        {/* AWS */}
+        <SkillChip
+          className="absolute top-20 right-0"
+          classes={{ img: 'relative top-1' }}
+          src="/img/aws-logo.svg"
+        />
+
+        {/* Node.js */}
+        <SkillChip
+          className="absolute top-64 left-0"
+          classes={{ img: 'relative top-1' }}
+          src="/img/nodejs-logo.svg"
+        />
+
+        {/* PostgreSQL */}
+        <SkillChip
+          className="absolute top-72 right-0"
+          classes={{ img: 'relative top-1' }}
+          src="/img/postgres-logo.svg"
+        />
       </div>
     </div>
   );
