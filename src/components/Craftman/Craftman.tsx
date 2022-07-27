4a5af9d81b2f60388a07a705b58ddf639e8d4442
Damien Monni <contact@damien-monni.com>
2022-07-27 19:05:28 +0200

    Add a Craftman section bellow the Hero section

---
+++ src/components/Craftman/Craftman.tsx
@@ -0,0 +1,32 @@
+import clsx from 'clsx';
+
+interface CraftmanProps {
+  className?: string;
+}
+
+export default function Craftman({ className }: CraftmanProps) {
+  return (
+    <section
+      className={clsx(
+        'bg-primary-text text-white px-20 py-40 text-center',
+        className,
+      )}
+    >
+      <h2>Artisan logiciel passionné</h2>
+
+      <div className="max-w-2xl mx-auto">
+        <p className="text-xl mt-16 mb-10">
+          Developpeur passionné, je créé du code de qualité, lisible, testé et
+          simple à maintenir.
+        </p>
+
+        <p className="text-xl">
+          J’adère entièrement à la philosophie du{' '}
+          <strong>Software Craftsmanship</strong>, afin de vous livrer un
+          travail sur lequel vous pourrez évoluer en toute sereinité pour la
+          suite de votre projet.
+        </p>
+      </div>
+    </section>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next';
+import Craftman from '../components/Craftman/Craftman';
 import Hero from '../components/Hero/Hero';
 import Topbar from '../components/Topbar/Topbar';
 
@@ -7,8 +8,13 @@ const Home: NextPage = () => {
     <>
       <Topbar className="fixed top-0 right-0 left-0" />
 
-      <div className="bg-primary-light">
-        <Hero className="pt-40 pb-28 max-w-6xl mx-auto" />
+      <div className="bg-primary-light pt-40 pb-32">
+        <Hero className="pb-28 max-w-6xl mx-auto" />
+
+        {/* We make the Craftman section slightly wider than the Hero */}
+        <div className="max-w-6xl mx-auto">
+          <Craftman className="-mx-20" />
+        </div>
       </div>
     </>
   );
