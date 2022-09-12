9515f9d3f84fcb066d437c6943c23750c6a65a8f
Damien Monni <contact@damien-monni.com>
2022-09-12 09:46:25 +0200

    Update texts

--- src/components/Projects/Sourcin.tsx
+++ src/components/Projects/Sourcin.tsx
@@ -20,22 +20,35 @@ export default function Sourcin({ className }: SourcinProps) {
       className={className}
     >
       <p>
-        Soucin est une entreprise proposant des formations en ligne réalisées
-        sur mesure pour chacun de ses clients qui sont majoritairement dans
-        l’industrie biopharmaceutique.
+        Sourcin créer et héberge des contenus de formations sur mesure pour
+        l'industrie biopharmaceutique.
       </p>
 
       <p>
-        Depuis plusieurs années, leur plateforme utilisait un système de gestion
-        de contenu (CMS) appelé Liferay qui devenait limittant. Ils m’ont ainsi
-        demandé de migrer l’application vers un ensemble de technlogies plus
-        moderne : React et AWS (Amazon Web Services).
+        Les contenus peuvent être sous plusieurs formats : vidéos, page web
+        interactive, quiz, PDF...
       </p>
 
       <p>
-        Ensemble et avec un prestataire spécialiste du cloud AWS nous avons
-        ainsi développé une nouvelle version de la plateforme qui est
-        actuellement (début 2020) en cours de migration.
+        Le domaine de la biopharmaceutique demande une grande maitrise de
+        l'infrastructure et des données de la plateforme, ce qui leur impose de
+        développer leur propre plateforme d'hébergement et de distribution de
+        leurs contenus de formation.
+      </p>
+
+      <p>
+        Sur ce projet long terme, mon rôle a été pendant 4 ans de déveloper une
+        grande partie de la plateforme, parfois seul, parfois en équipe, de
+        définir l'infrastructure, de la sécuriser et d'avoir un rôle de lead
+        technique lors des phase de développement en équipe.
+      </p>
+
+      <p>
+        La stack est composée d'un front-end React et d'un backend 100%
+        Serverless hébergé sur AWS. Le backend utilise entre autres les services
+        AWS Lambda, S3, API Gateway, Cognito, DynamoDB et Aurora PostgreSQL
+        Serverless. L'infrastructure est déclarée par code (Infrastructure as
+        Code) avec <em>Serverless Framework</em>.
       </p>
     </Project>
   );
