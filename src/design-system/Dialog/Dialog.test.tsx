0f630baa79105f3fbd598c9a90f5c37636788479
Damien Monni <contact@damien-monni.com>
2022-09-12 10:47:06 +0200

    Create a Dialog component

---
+++ src/design-system/Dialog/Dialog.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import SystemDialog from './Dialog';
+
+// @ts-expect-error
+global.IntersectionObserver = class FakeIntersectionObserver {
+  observe() {}
+  disconnect() {}
+};
+
+test('A click on the close button closes the Dialog', async () => {
+  const user = userEvent.setup();
+
+  const onClose = jest.fn();
+  render(<SystemDialog title="My title" open onClose={onClose} />);
+
+  const closeButton = screen.getByText(/fermer/i);
+  await user.click(closeButton);
+
+  expect(onClose).toHaveBeenCalledTimes(1);
+});
+
+test('The title renders in the dialog', () => {
+  render(<SystemDialog open title="My title" onClose={jest.fn()} />);
+  expect(screen.getByText('My title')).toBeInTheDocument();
+});
