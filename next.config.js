eb4e0b1a3ba3ebee589d5debf61027d206722559
Damien Monni <contact@damien-monni.com>
2023-01-27 14:35:19 +0100

    Start to setup Next.js internationalization

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ['fr', 'en'],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: 'fr',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
