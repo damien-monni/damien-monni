10cf89816867e963b75687ad23dfb5c4883ec52b
Damien Monni <contact@damien-monni.com>
2023-02-03 12:03:48 +0100

    Translate the introduction of Services section in english

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -5,21 +5,21 @@ import Frontend from './Frontend';
 import Serverless from './Serverless';
 import Testing from './Testing';
 import Typescript from './Typescript';
+import { useTranslation } from 'next-i18next';
 
 interface ServicesProps {
   className?: string;
 }
 
 export default function Services({ className }: ServicesProps) {
+  const { t } = useTranslation();
+
   return (
     <section className={clsx(className)}>
-      <h2 className="text-center">Services</h2>
+      <h2 className="text-center">{t('Services.title')}</h2>
 
       <p className="text-center text-lg my-12 max-w-2xl mx-4 sm:mx-auto sm:my-12 sm:text-xl">
-        Mes expériences variées me permettent d’intervenir sur différents
-        domaines d’expertise. Ainsi, je peux aussi bien venir renforcer votre
-        équipe sur un domaine spécifique qu’intervenir en tant que full-stack
-        sur toute votre application.
+        {t('Services.description')}
       </p>
 
       <div className="grid gap-4 sm:grid-cols-2 sm:gap-10 lg:grid-cols-3 lg:gap-20">
