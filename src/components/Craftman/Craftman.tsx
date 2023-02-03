7f39084e193f78bae4f35903442571308f083944
Damien Monni <contact@damien-monni.com>
2023-02-03 11:58:01 +0100

    Translate the software craftman section to english

--- src/components/Craftman/Craftman.tsx
+++ src/components/Craftman/Craftman.tsx
@@ -1,10 +1,12 @@
 import clsx from 'clsx';
-
+import { Trans, useTranslation } from 'next-i18next';
 interface CraftmanProps {
   className?: string;
 }
 
 export default function Craftman({ className }: CraftmanProps) {
+  const { t } = useTranslation();
+
   return (
     <section
       className={clsx(
@@ -12,19 +14,20 @@ export default function Craftman({ className }: CraftmanProps) {
         className,
       )}
     >
-      <h2>Artisan logiciel passionné</h2>
+      <h2>{t('Craftman.title')}</h2>
 
       <div className="max-w-2xl mx-auto">
         <p className="text-lg lg:text-xl mt-8 mb-6 lg:mt-16 lg:mb-10">
-          Développeur passionné, je créé du code de qualité, lisible, testé et
-          facile à faire évoluer.
+          {t('Craftman.passionate')}
         </p>
 
         <p className="text-lg lg:text-xl">
-          J’adhère entièrement à la philosophie du{' '}
-          <strong>Software Craftsmanship</strong>, afin de vous livrer un
-          travail sur lequel vous pourrez évoluer en toute sérénité pour la
-          suite de votre projet.
+          <Trans i18nKey="Craftman.details">
+            J’adhère entièrement à la philosophie du{' '}
+            <strong>Software Craftsmanship</strong>, afin de vous livrer un
+            travail sur lequel vous pourrez évoluer en toute sérénité pour la
+            suite de votre projet.
+          </Trans>
         </p>
       </div>
     </section>
