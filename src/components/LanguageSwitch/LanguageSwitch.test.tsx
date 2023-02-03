c5472e6959b0dd89fae6e69d3ce0af4a65acf535
Damien Monni <contact@damien-monni.com>
2023-02-03 14:28:57 +0100

    Add tests for better coverage

--- src/components/LanguageSwitch/LanguageSwitch.test.tsx
+++ src/components/LanguageSwitch/LanguageSwitch.test.tsx
@@ -1,6 +1,51 @@
-import { render } from '@test-utils';
+import { render, screen } from '@test-utils';
 import { LanguageSwitch } from './LanguageSwitch';
+import userEvent from '@testing-library/user-event';
+import { useRouter } from 'next/router';
+
+const user = userEvent.setup();
+
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(() => ({
+    locale: 'en',
+    push: jest.fn(),
+  })),
+}));
 
 test('render the LanguageSwitch component', () => {
   render(<LanguageSwitch />);
+});
+
+test('switch language from en to fr', async () => {
+  const mockUseRouter = useRouter as jest.Mock;
+  const routerPush = jest.fn();
+  mockUseRouter.mockReturnValue({ locale: 'en', push: routerPush });
+
+  render(<LanguageSwitch />);
+  const button = screen.getByRole('button');
+  await user.click(button);
+
+  expect(routerPush).toHaveBeenCalledTimes(1);
+  expect(routerPush).toHaveBeenCalledWith(
+    { pathname: undefined, query: undefined },
+    undefined,
+    { locale: 'fr' },
+  );
+});
+
+test('switch language from fr to en', async () => {
+  const mockUseRouter = useRouter as jest.Mock;
+  const routerPush = jest.fn();
+  mockUseRouter.mockReturnValue({ locale: 'fr', push: routerPush });
+
+  render(<LanguageSwitch />);
+  const button = screen.getByRole('button');
+  await user.click(button);
+
+  expect(routerPush).toHaveBeenCalledTimes(1);
+  expect(routerPush).toHaveBeenCalledWith(
+    { pathname: undefined, query: undefined },
+    undefined,
+    { locale: 'en' },
+  );
 });
--- src/components/Topbar/Topbar.test.tsx
+++ src/components/Topbar/Topbar.test.tsx
@@ -1,6 +1,49 @@
-import { render } from '@test-utils';
+import { render, screen } from '@test-utils';
+import { useRouter } from 'next/router';
 import Topbar from './Topbar';
 
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(() => ({
+    locale: 'en',
+  })),
+}));
+
 test('render the Topbar component', () => {
+  render(<Topbar />);
+});
+
+test('show a WIP warning message if the locale is english', async () => {
+  const mockUseRouter = useRouter as jest.Mock;
+  const routerPush = jest.fn();
+  mockUseRouter.mockReturnValue({ locale: 'en', push: routerPush });
+
+  render(<Topbar />);
+
+  expect(
+    screen.getByText(
+      /The English version of this website is currently a work in progress/,
+    ),
+  ).toBeInTheDocument();
+});
+
+test('Do not show a WIP warning message if the locale is french', () => {
+  const mockUseRouter = useRouter as jest.Mock;
+  const routerPush = jest.fn();
+  mockUseRouter.mockReturnValue({ locale: 'fr', push: routerPush });
+
+  render(<Topbar />);
+
+  expect(
+    screen.queryByText(
+      /The English version of this website is currently a work in progress/,
+    ),
+  ).not.toBeInTheDocument();
+});
+
+test('Handle undefined locale returned by useRouter', () => {
+  const mockUseRouter = useRouter as jest.Mock;
+  const routerPush = jest.fn();
+  mockUseRouter.mockReturnValue({ locale: undefined, push: routerPush });
+
   render(<Topbar />);
 });
